8024f15814319dc5490efc2dfb10f96fa0d3d817
impedrohenri <impedrohenri@gmail.com>
2026-01-15 03:15:23 -0300

    refactor: adapta layout do input Select/Picker para atender ao iOS

--- components/Select.tsx
+++ components/Select.tsx
@@ -1,8 +1,8 @@
 import { SelectProps } from "@/types/SelectProps";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Picker } from "@react-native-picker/picker";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Platform, Modal, TouchableOpacity } from "react-native";
 
 export default function Select({
   label,
@@ -13,11 +13,74 @@ export default function Select({
   leftIcon,
   placeholder,
 }: SelectProps) {
+
+  const [isModalVisible, setModalVisible] = useState(false);
+
   const pickerContainerStyle = [styles.pickerContainer, style];
 
   const pickerOptions = placeholder
     ? [{ label: placeholder, value: "" }, ...options]
     : options;
+
+  const selectedOption = pickerOptions.find((o) => o.value === selectedValue);
+
+  if (Platform.OS === "ios") return (
+    <>
+      <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={pickerContainerStyle} onTouchStart={() => setModalVisible(true)}>
+        {leftIcon && <View style={styles.iconContainer}>{leftIcon}</View>}
+        <View style={styles.pickerWrapper}>
+          <Text>{selectedOption?.label}</Text>
+          <MaterialIcons
+            name="arrow-drop-down"
+            size={24}
+            color="#666"
+            style={styles.dropdownIcon}
+          />
+        </View>
+      </View>
+
+      <Modal
+        visible={isModalVisible}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalOverlayIOS} onTouchStart={() => setModalVisible(false)}>
+        </View>
+        <View style={styles.pickerHeaderIOS}>
+          <MaterialIcons name="close" size={24} color="#007AFF" style={{paddingInline: 10}} onTouchStart={() => setModalVisible(false)}/>
+        </View>
+        <View style={styles.pickerWrapperIOS}>
+          <Picker 
+            selectedValue={selectedValue}
+            onValueChange={onValueChange}          
+            style={styles.pickerIOS}>
+          {pickerOptions.map((option) => (
+            <Picker.Item
+              key={option.value}
+              label={option.label}
+              value={option.value}
+              color={option.value === "" ? "#999" : "#424242"}
+            />
+          ))}
+        </Picker>
+        </View>
+        <View style={styles.pickerFooterIOS}>
+          <TouchableOpacity
+              onPress={() => setModalVisible(false)}
+              hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+            >
+              <Text style={styles.doneButtonText}>Concluído</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={styles.modalOverlayIOS} onTouchStart={() => setModalVisible(false)}>
+        </View>
+      </Modal>
+    </View>
+    </>
+  )
 
   return (
     <View style={styles.container}>
@@ -25,6 +88,7 @@ export default function Select({
       <View style={pickerContainerStyle}>
         {leftIcon && <View style={styles.iconContainer}>{leftIcon}</View>}
         <View style={styles.pickerWrapper}>
+          
           <Picker
             selectedValue={selectedValue}
             onValueChange={onValueChange}
@@ -97,5 +161,41 @@ const styles = StyleSheet.create({
   iconContainer: {
     paddingLeft: 16,
     paddingRight: 8,
+  },
+
+
+  // IOS
+  modalOverlayIOS: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+  },
+  pickerIOS: {
+    backgroundColor: "#fff",
+    elevation: 4,
+  },
+  pickerWrapperIOS: {
+    backgroundColor: "#e5e5e5",
+    paddingVertical: 1,
+  },
+  pickerHeaderIOS: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    height: 40,
+  },
+  pickerFooterIOS: {
+    height: 44,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    backgroundColor: "#fff",
+  },
+  doneButtonText: {
+    color: "#007AFF",
+    fontWeight: "600",
+    fontSize: 18,
   },
 });
